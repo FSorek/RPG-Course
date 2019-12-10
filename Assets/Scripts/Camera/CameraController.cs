e928277176166fd8f2bda6da5887d4bf07494fd0
fsorek <filip.sorek96@gmail.com>
2019-12-11 00:08:12 +0100

    Camera Controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float tilt;
+
+    private void Update()
+    {
+        float mouseRotation = Input.GetAxis("Mouse Y");
+        tilt = Mathf.Clamp(tilt - mouseRotation, -15f, 15f);
+        transform.localRotation = Quaternion.Euler(tilt, 0,0);
+    }
+}
