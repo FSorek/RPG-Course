e2f091781770bbd09b64bb56d06853fcd47e225c
fsorek <filip.sorek96@gmail.com>
2020-06-15 21:29:03 +0200

    17b First Test First

---
+++ Assets/PlayTests/inventory_panel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PlayTests
+{
+    public class inventory_panel
+    {
+        [Test]
+        public void has_25_slots()
+        {
+            var inventoryPanel = GetInventoryPanel();
+            Assert.AreEqual(25, inventoryPanel.SlotCount);
+            
+        }
+
+        private UIInventoryPanel GetInventoryPanel()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<UIInventoryPanel>("Assets/Prefabs/UI/InventoryPanel.prefab");
+            return Object.Instantiate(prefab);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIInventoryPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPanel : MonoBehaviour
+{
+    private UIInventorySlot[] slots;
+
+    private void Awake()
+    {
+        slots = GetComponentsInChildren<UIInventorySlot>();
+    }
+
+    public int SlotCount => slots.Length;
+}
