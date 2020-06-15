8aad1c16e5ff4680ecbd8d978915e84640e0e436
fsorek <filip.sorek96@gmail.com>
2020-06-15 20:30:34 +0200

    16c Bootstrapper

---
+++ Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrapper : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    public static void Initialize()
+    {
+        var inputGameObject = new GameObject("[INPUT SYSTEM]");
+        inputGameObject.AddComponent<PlayerInput>();
+        GameObject.DontDestroyOnLoad(inputGameObject);
+    }
+}
