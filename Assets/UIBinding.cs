1938156f1dcbdf920598c54ae701252a00e2f02e
fsorek <filip.sorek96@gmail.com>
2020-06-15 22:02:10 +0200

    Binding to the Player

---
+++ Assets/UIBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBinding : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        var player = FindObjectOfType<Player>();
+        while (player == null)
+        {
+            yield return null;
+            player = FindObjectOfType<Player>();
+        }
+        
+        GetComponent<UIInventoryPanel>().Bind(player.GetComponent<Inventory>());
+    }
+}
