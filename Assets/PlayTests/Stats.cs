3e4713a366e0b1a0b12f303defc18283e4cdd62e
fsorek <filip.sorek96@gmail.com>
2020-06-17 01:35:51 +0200

    21b Simple Tested Stats

---
+++ Assets/PlayTests/Stats.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+public class stats
+{
+    [Test]
+    public void can_add()
+    {
+        Stats stats = new Stats();
+        stats.Add(StatType.MoveSpeed, 3f);
+        Assert.AreEqual(3f, stats.Get(StatType.MoveSpeed));
+        stats.Add(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(8f, stats.Get(StatType.MoveSpeed));
+    }
+    
+    [Test]
+    public void can_remove()
+    {
+        Stats stats = new Stats();
+        stats.Add(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(5f, stats.Get(StatType.MoveSpeed));
+        
+        stats.Remove(StatType.MoveSpeed, 2f);
+        Assert.AreEqual(3f, stats.Get(StatType.MoveSpeed));
+    }
+}
---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class Stats
+{
+    private Dictionary<StatType, float> stats = new Dictionary<StatType, float>();
+
+    public void Add(StatType statType, float value)
+    {
+        if (stats.ContainsKey(statType))
+            stats[statType] += value;
+        else
+            stats[statType] = value;
+    }
+
+    public float Get(StatType statType)
+    {
+        return stats[statType];
+    }
+
+    public void Remove(StatType statType, float value)
+    {
+        if (stats.ContainsKey(statType))
+            stats[statType] -= value;
+    }
+}
